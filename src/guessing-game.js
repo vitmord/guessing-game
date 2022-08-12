e8d682f326fb6c4843c35b438bc880f0efd41685
vitmord <vitmord@gmail.com>
2022-08-12 14:02:28 +0300

    feat: add solution

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -1,20 +1,26 @@
 class GuessingGame {
-    constructor() {}
+    constructor() {
+        this.num = Math.ceil(this.max / 2);
+        this.min;
+        this.max;
+    }
 
     setRange(min, max) {
-
+        this.min = min;
+        this.max = max;
     }
 
     guess() {
-
+        this.num = Math.ceil((this.max - this.min) / 2 + this.min);
+        return this.num;
     }
 
     lower() {
-
+        this.setRange(this.min, this.num);
     }
 
     greater() {
-
+        this.setRange(this.num, this.max);
     }
 }
 
